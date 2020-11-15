c56d2cfe80d02f8f1cfdcaf49536e814a3317833
Bernard Telles <bernardtelgon@gmail.com>
2020-11-14 23:50:27 -0300

    new file

---
+++ pasta 1/script1.js
@@ -0,0 +1,14 @@
+var possuiGraduacao = false;
+var possuiDoutorado = true;
+
+if(possuiGraduacao) {
+  console.log( 'é verdadeiro');
+  var x = 10;
+  
+} else if(possuiDoutorado) {
+  console.log('possui doutorado');
+} else{
+  console.log('nao possui nada');
+}
+
+console.log(x);
